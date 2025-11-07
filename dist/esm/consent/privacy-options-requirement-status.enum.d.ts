c5e38d2dd94d38b3c89fb60c91eebf426758c6d9
adrienj <eyospace@gmail.com>
2025-11-07 21:02:38 +0100

    build

--- dist/esm/consent/privacy-options-requirement-status.enum.d.ts
+++ dist/esm/consent/privacy-options-requirement-status.enum.d.ts
@@ -4,16 +4,16 @@
  *
  * */
 export declare enum PrivacyOptionsRequirementStatus {
-  /**
-   * Privacy options entry point is not required.
-   */
-  NOT_REQUIRED = 'NOT_REQUIRED',
-  /**
-   * Privacy options entry point is required.
-   */
-  REQUIRED = 'REQUIRED',
-  /**
-   * Privacy options requirement status is unknown.
-   */
-  UNKNOWN = 'UNKNOWN',
+    /**
+     * Privacy options entry point is not required.
+     */
+    NOT_REQUIRED = "NOT_REQUIRED",
+    /**
+     * Privacy options entry point is required.
+     */
+    REQUIRED = "REQUIRED",
+    /**
+     * Privacy options requirement status is unknown.
+     */
+    UNKNOWN = "UNKNOWN"
 }
